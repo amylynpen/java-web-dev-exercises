9f930fe5627b5823d268f6a03e01ec0b910fa881
Amy <amylynnroberts@gmail.com>
2021-09-02 21:09:42 -0500

    studio working

---
+++ src/org/launchcode/java/studios/areaofacircle/Circle.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.studios.areaofacircle;
+
+//package.src
+public class Circle {
+
+    public static Double getArea(Double radius) {
+        return 3.14 * radius * radius;
+    }
+
+}
