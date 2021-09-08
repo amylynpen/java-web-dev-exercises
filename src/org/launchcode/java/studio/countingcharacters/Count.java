23a7b80147608e021256de1cdac4095908baf565
Amy <amylynnroberts@gmail.com>
2021-09-07 20:56:15 -0500

    studio 3.2

--- src/org/launchcode/java/studio/countingcharacters/Count.java
+++ src/org/launchcode/java/studio/countingcharacters/Count.java
@@ -9,7 +9,7 @@ public class Count {
     public static void main(String [] args) {
 
         HashMap<Character, Integer> charCount = new HashMap<>();
-        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there";;
+        String myString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there";
 
         char[] charactersInString = myString.toCharArray();
 
@@ -31,7 +31,5 @@ public class Count {
         }
     }
 
-    {
 
-    }
 }
